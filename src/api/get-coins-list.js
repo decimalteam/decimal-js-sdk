4b9b0b069ce25171f396f3a480d3b2a3f3ecccb6
sem <shnurkov.sem@gmail.com>
2020-06-15 17:01:01 +0700

    coins list upgrade

--- src/api/get-coins-list.js
+++ src/api/get-coins-list.js
@@ -2,7 +2,14 @@
 export default function getCoinsList(api) {
   return async (limit, offset, query) => {
     const url = '/coin';
-    const { data } = await api.get(url, { params: { limit, offset, query } });
+    const params = {
+      limit,
+      offset,
+    };
+    if (query) {
+      params.query = query;
+    }
+    const { data } = await api.get(url, { params });
     return data.result;
   };
 }
