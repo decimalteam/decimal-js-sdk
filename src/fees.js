df4e48ef4e4960d91d972120d4480ce72a2c28aa
sem <shnurkov.sem@gmail.com>
2020-06-19 16:22:20 +0700

    create coin fee fix

--- src/fees.js
+++ src/fees.js
@@ -30,32 +30,6 @@ FEES[TX_TYPE.MULTISIG_CREATE_WALLET] = 100;
 FEES[TX_TYPE.MULTISIG_CREATE_TX] = 100;
 FEES[TX_TYPE.MULTISIG_SIGN_TX] = 100;
 
-function getCommissionForCreateCoin(ticker) {
-  let baseCoinFee = 0;
-  switch (ticker.length) {
-    case 3:
-      baseCoinFee = 1000000;
-      break;
-    case 4:
-      baseCoinFee = 100000;
-      break;
-    case 5:
-      baseCoinFee = 10000;
-      break;
-    case 6:
-      baseCoinFee = 1000;
-      break;
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-      baseCoinFee = 100;
-      break;
-    default:
-      return new Error('Invalid ticker length');
-  }
-  return baseCoinFee / unit;
-}
 
 function getAmountFromSatoshi(amount) {
   return new Decimaljs(amount).times(new Decimaljs(10).pow(-18)).toFixed(0);
@@ -83,41 +57,13 @@ function getCoinPrice(coin) {
   return result;
 }
 
-// eslint-disable-next-line no-unused-vars
-function getStringMemorySize(_string) {
-  let codePoint;
-  let accum = 0;
-  for (let stringIndex = 0, endOfString = _string.length; stringIndex < endOfString; stringIndex++) {
-    codePoint = _string.charCodeAt(stringIndex);
-
-    if (codePoint < 0x100) {
-      accum += 1;
-      continue;
-    }
-
-    if (codePoint < 0x10000) {
-      accum += 2;
-      continue;
-    }
-
-    if (codePoint < 0x1000000) {
-      accum += 3;
-    } else {
-      accum += 4;
-    }
-  }
-
-  return accum * 2;
-}
-
 export default function getCommission(api) {
   return async (tx) => {
     const { type } = tx.msg[0];
     const ticker = tx.fee.amount[0].denom || 'tdel';
     const textSize = toCanonicalJSONBytes(tx).length;
     const feeForText = new Decimaljs(textSize).times(2).times(unit);
-    const fixedFee = type === FEES[type];
-    const feeInBase = new Decimaljs(unit).times(fixedFee).plus(feeForText);
+    const feeInBase = new Decimaljs(unit).times(FEES[type]).plus(feeForText);
 
     if (ticker !== 'tdel') {
       const coin = await getCoin(api)(ticker);
--- src/test/main.js
+++ src/test/main.js
@@ -20,20 +20,34 @@ const wallet = new Wallet('hollow luggage slice soup leg vague icon walnut sessi
 
   // const test = await decimal.redeemCheck(txParams, wallet);
   // console.log(test);
+  // const txParams = {
+  //   data: {
+  //     sender: wallet.address,
+  //     receiver: wallet.address,
+  //     coin: {
+  //       amount: '50000000000000000000',
+  //       denom: 'tdel',
+  //     },
+  //   },
+  //   gas: '200000',
+  //   message: '11111111111'
+  // };
+
   const txParams = {
     data: {
       sender: wallet.address,
-      receiver: wallet.address,
-      coin: {
-        amount: '50000000000000000000',
-        denom: 'tdel',
-      },
+      title: 'Test coin',
+      symbol: 'feeeeee',
+      constant_reserve_ratio: '45',
+      initial_volume: '1000000000000000000',
+      initial_reserve: '10000000000000000000000',
+      limit_volume: '1000000000000000000000000000',
     },
     gas: '200000',
-    message: '11111111111'
-  };
+    message: 'message'
+  }
 
-  const fee = await decimal.estimateTxCommission(TX_TYPE.COIN_REDEEM_CHECK, txParams, wallet);
+  const fee = await decimal.estimateTxCommission(TX_TYPE.COIN_CREATE, txParams, wallet);
   console.log(111, fee);
-  await decimal.sendCoins(txParams, wallet);
+  await decimal.createCoin(txParams, wallet);
 }());
