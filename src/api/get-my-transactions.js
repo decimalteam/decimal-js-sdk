71d781e363dabcc80618f38427312a2056d7526c
sshnur <shnurkov.sem@gmail.com>
2020-07-31 21:10:12 +0700

    improve getMyTransactions method

--- src/api/get-my-transactions.js
+++ src/api/get-my-transactions.js
@@ -1,7 +1,11 @@
 export default function getMyTransactions(api, wallet) {
-  return async (limit = 10, offset = 0, types) => {
+  return async (limit = 10, offset = 0, types, coins) => {
     const url = `/address/${wallet.address}/txs`;
-    const { data } = await api.get(url, { params: { limit, offset, types } });
+    const { data } = await api.get(url, {
+      params: {
+        limit, offset, types, coins,
+      },
+    });
     return data.result;
   };
 }
