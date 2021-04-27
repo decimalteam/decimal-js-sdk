85bd7433d0a0129641c55b1622d4d256a6028d1b
qoelk <qoelky@zohomail.com>
2021-04-27 17:30:04 +0700

    upd: add missing fee param

--- src/fees.js
+++ src/fees.js
@@ -37,6 +37,10 @@ FEES[TX_TYPE.SWAP_HTLT] = 33000;
 FEES[TX_TYPE.SWAP_REDEEM] = 0;
 FEES[TX_TYPE.SWAP_REFUND] = 0;
 FEES[TX_TYPE.COIN_UPDATE] = 0;
+FEES[TX_TYPE.NFT_MINT] = 0;
+FEES[TX_TYPE.NFT_BURN] = 0;
+FEES[TX_TYPE.NFT_EDIT_METADATA] = 0;
+FEES[TX_TYPE.NFT_TRANSFER] = 0;
 
 async function getCoinPrice(api, ticker) {
   const coin = await getCoin(api)(ticker);
