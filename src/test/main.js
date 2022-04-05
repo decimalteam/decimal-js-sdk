b6739f1c9b87fdb06c3be39583453748d5978414
Max Brodt <maxbroqt@gmail.com>
2022-04-05 13:45:43 +0700

    feat(): removed methods from readme

--- src/test/main.js
+++ src/test/main.js
@@ -1,12 +1,6 @@
 /* eslint-disable */
-import { Wallet, Decimal, TX_TYPE } from '../index';
-import TYPE_TX from '../txTypes';
+import { Wallet, Decimal } from '../index';
 import {data, options} from './data';
-import {makeSignature, prepareTx, postTx} from '../txUtils';
-import {createBroadcastTx} from '@tendermint/sig';
-import { getTransaction } from '../tx';
-import { generateNftId } from '../utils';
-import * as fs from 'fs';
 
 const mnemonic = '';
 
@@ -17,8 +11,4 @@ const gateUrl = 'https://devnet-gate.decimalchain.com/api/';
 const decimal = new Decimal({ gateUrl, wallet, });
 
 (async () => {
-  // const res = await decimal.updateAddressBlockingData('dx1lx4lvt8sjuxj8vw5dcf6knnq0pacre4w6hdh2v', true, 'both', 'gay');
-  const res = await decimal.getBlockedAddresses(10, 0, 'incoming');
-  
-  console.info(res);
 })();
